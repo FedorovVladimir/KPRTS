1c4834b30bb249d4200d3dca0cb0a9489732cbe8
Vladimir <vladimir.fodorow@gmail.com>
2019-04-20 11:32:34 +0700

    Тест на включение пульта

--- src/test/java/ButtonsTest.java
+++ src/test/java/ButtonsTest.java
@@ -1,7 +1,16 @@
 import model.KPRTS;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
 
 public class ButtonsTest {
 
     private KPRTS kprts = new KPRTS();
+
+    @Test
+    public void turnOn() {
+        kprts.turnOn();
+        assertTrue(kprts.isTurn());
+    }
 
 }
