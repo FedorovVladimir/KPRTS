72fe5a5a24193a64bd29e71981f14516b3ef87d9
Vladimir <https://github.com/FedorovVladimir/Write_Java>
2019-04-21 18:34:28 +0700

    Тест на включение пульта пройден

--- src/main/java/model/KPRTS.java
+++ src/main/java/model/KPRTS.java
@@ -7,9 +7,20 @@ public class KPRTS {
 
     private boolean isOn = false;
 
+    public KPRTS() {
+        isOnLight.put(Light.APK, true);
+        isOnLight.put(Light.MB1, true);
+    }
+
     private Map<Buttons, Boolean> isOnButtons = new HashMap<Buttons, Boolean>(){{
         for (Buttons button : Buttons.values()) {
             put(button, false);
+        }
+    }};
+
+    private Map<Light, Boolean> isOnLight = new HashMap<Light, Boolean>(){{
+        for (Light light : Light.values()) {
+            put(light, false);
         }
     }};
 
@@ -36,10 +47,11 @@ public class KPRTS {
     }
 
     public boolean isOn(Light light) {
-        return true;
+        return isOnLight.get(light);
     }
 
     public void turnOff() {
-
+        isOnLight.put(Light.APK, false);
+        isOnLight.put(Light.MB1, false);
     }
 }
