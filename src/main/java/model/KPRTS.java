92e04e116055ccf63ef76adcbc12f6230236d406
Vladimir <https://github.com/FedorovVladimir/Write_Java>
2019-04-21 18:58:08 +0700

    Удалена фозможность включения пнопки, теперь можно нажать

--- src/main/java/model/KPRTS.java
+++ src/main/java/model/KPRTS.java
@@ -12,12 +12,6 @@ public class KPRTS {
         isOnLight.put(Light.MB1, true);
     }
 
-    private Map<Buttons, Boolean> isOnButtons = new HashMap<Buttons, Boolean>(){{
-        for (Buttons button : Buttons.values()) {
-            put(button, false);
-        }
-    }};
-
     private Map<Light, Boolean> isOnLight = new HashMap<Light, Boolean>(){{
         for (Light light : Light.values()) {
             put(light, false);
@@ -32,24 +26,23 @@ public class KPRTS {
         return isOn;
     }
 
-    public void click(Buttons button) {
+    public boolean click(Buttons button) {
         if (isOn) {
-            isOnButtons.put(button, !isOnButtons.get(button));
-        }
-    }
 
-    public boolean isOn(Buttons button) {
-        return isOnButtons.get(button);
+        }
+        return false;
     }
 
     public boolean isOn(Light light) {
-        return isOnLight.get(light);
+        if (isOn) {
+            return isOnLight.get(light);
+        } else {
+            return false;
+        }
     }
 
     public void turnOff() {
         isOn = false;
-        isOnLight.put(Light.APK, false);
-        isOnLight.put(Light.MB1, false);
     }
 
     public Panel getPanel(int i) {
--- src/test/java/KPRTSTest.java
+++ src/test/java/KPRTSTest.java
@@ -13,50 +13,43 @@ class KPRTSTest {
     void turnOn() {
         kprts.turnOn();
         assertTrue(kprts.isTurn());
-        assertTrue(kprts.isOn(Light.APK));
-        assertTrue(kprts.isOn(Light.MB1));
         assertEquals("   150,0", kprts.getPanel(1).getText());
         assertEquals("   150,0", kprts.getPanel(2).getText());
         assertEquals("комп тлф", kprts.getPanel(3).getText());
+        assertTrue(kprts.isOn(Light.APK));
         assertEquals("   118,0", kprts.getPanel(4).getText());
         assertEquals("   118,0", kprts.getPanel(5).getText());
         assertEquals("пш      ", kprts.getPanel(6).getText());
+        assertTrue(kprts.isOn(Light.MB1));
+
         kprts.turnOff();
-        assertFalse(kprts.isOn(Light.APK));
-        assertFalse(kprts.isOn(Light.MB1));
+        assertFalse(kprts.isTurn());
         assertEquals("        ", kprts.getPanel(1).getText());
         assertEquals("        ", kprts.getPanel(2).getText());
         assertEquals("        ", kprts.getPanel(3).getText());
+        assertFalse(kprts.isOn(Light.APK));
         assertEquals("        ", kprts.getPanel(4).getText());
         assertEquals("        ", kprts.getPanel(5).getText());
         assertEquals("        ", kprts.getPanel(6).getText());
+        assertFalse(kprts.isOn(Light.MB1));
     }
 
     @Test
     void clickApk_On() {
         kprts.turnOn();
-        kprts.click(Buttons.APK);
-        assertTrue(kprts.isOn(Buttons.APK));
-        kprts.click(Buttons.APK);
-        assertFalse(kprts.isOn(Buttons.APK));
+        assertTrue(kprts.click(Buttons.APK));
     }
 
     @Test
     void clickApk_Off() {
-        kprts.click(Buttons.APK);
-        assertFalse(kprts.isOn(Buttons.APK));
-        kprts.click(Buttons.APK);
-        assertFalse(kprts.isOn(Buttons.APK));
+        assertFalse(kprts.click(Buttons.APK));
     }
 
     @Test
     void clickAllButton() {
         kprts.turnOn();
         for (Buttons button: Buttons.values()) {
-            kprts.click(button);
-            assertTrue(kprts.isOn(button));
-            kprts.click(button);
-            assertFalse(kprts.isOn(button));
+            assertTrue(kprts.click(button));
         }
     }
 }
