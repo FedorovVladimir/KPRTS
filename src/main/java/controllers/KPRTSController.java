7ded72a1e9e51d2824b8f9a50a407077c9cdcc90
user-penguin <kobzev96@gmail.com>
2019-05-12 02:45:07 +0700

    create setMethod to change text on panels

--- src/main/java/controllers/KPRTSController.java
+++ src/main/java/controllers/KPRTSController.java
@@ -4,6 +4,8 @@ import controllers.png_utils.LetterConverter;
 import javafx.fxml.FXML;
 import javafx.scene.image.ImageView;
 
+import java.util.ArrayList;
+
 
 public class KPRTSController {
     // показания основной и резервной/зпу навигации
@@ -43,41 +45,47 @@ public class KPRTSController {
     private ImageView contactMode_1, contactMode_2, contactMode_3, contactMode_4,
             contactMode_5, contactMode_6, contactMode_7, contactMode_8;
 
+    private ArrayList<ImageView[]> panels;
+
     @FXML
     public void initialize() {
-//        new LetterConverter().getImageByLetter('1', mainNavigation);
-        ImageView[] mainNav = new ImageView[] {
-                mainNavigation_1, mainNavigation_2, mainNavigation_3,
-                mainNavigation_4, mainNavigation_5, mainNavigation_6,
-                mainNavigation_7, mainNavigation_8
+        panels = new ArrayList<>();
+        ImageView[] mainNavigation = new ImageView[] {
+                mainNavigation_1, mainNavigation_2, mainNavigation_3, mainNavigation_4,
+                mainNavigation_5, mainNavigation_6,mainNavigation_7, mainNavigation_8
         };
-        ImageView[] reserNav = new ImageView[] {
+
+        ImageView[] reserveNavigation = new ImageView[] {
                 reserveNavigation_1, reserveNavigation_2, reserveNavigation_3,
                 reserveNavigation_4, reserveNavigation_5, reserveNavigation_6, reserveNavigation_7, reserveNavigation_8
         };
-        ImageView[] modeNav = new ImageView[] {
+        ImageView[] modeNavigation = new ImageView[] {
                 navigationMode_1, navigationMode_2, navigationMode_3, navigationMode_4,
                 navigationMode_5, navigationMode_6, navigationMode_7, navigationMode_8
         };
-        ImageView[] contMain = new ImageView[] {
+        ImageView[] mainContact = new ImageView[] {
                 contactMain_1, contactMain_2, contactMain_3, contactMain_4,
                 contactMain_5, contactMain_6, contactMain_7, contactMain_8
         };
-        ImageView[] contRes = new ImageView[] {
+        ImageView[] reserveContact = new ImageView[] {
                 contactReserve_1, contactReserve_2, contactReserve_3, contactReserve_4,
                 contactReserve_5, contactReserve_6, contactReserve_7, contactReserve_8
         };
-        ImageView[] contMod = new ImageView[] {
+        ImageView[] modeContact = new ImageView[] {
                 contactMode_1, contactMode_2, contactMode_3, contactMode_4,
                 contactMode_5, contactMode_6, contactMode_7, contactMode_8
         };
+        panels.add(mainNavigation);
+        panels.add(reserveNavigation);
+        panels.add(modeNavigation);
+        panels.add(mainContact);
+        panels.add(reserveContact);
+        panels.add(modeContact);
+    }
 
-        new LetterConverter().setTextInPanel(mainNav, "150,0");
-        new LetterConverter().setTextInPanel(reserNav, "КУ.КУ");
-        new LetterConverter().setTextInPanel(modeNav, "АААА");
-        new LetterConverter().setTextInPanel(contMain, "ОГА,ПОКА");
-        new LetterConverter().setTextInPanel(contRes, "1111");
-        new LetterConverter().setTextInPanel(contMod, "МЕМ");
+    public void setTextOnPanel(int numberOfPanel, String message) {
+        int concreteNum = numberOfPanel - 1;
+        new LetterConverter().setTextInPanel(panels.get(concreteNum), message);
     }
 
     public double getNavigationMain() {
